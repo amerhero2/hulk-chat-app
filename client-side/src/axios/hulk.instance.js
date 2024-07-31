caedf0a16ac37d84b7b404077d3a953fb22a54d4
amerhero2 <amerfsk@gmail.com>
2024-07-31 20:03:59 +0200

    add ternary operator

--- client-side/src/axios/hulk.instance.js
+++ client-side/src/axios/hulk.instance.js
@@ -28,7 +28,7 @@ instance.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
